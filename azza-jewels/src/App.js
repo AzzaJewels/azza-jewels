51e426805512f0cb22a54401b4933e7fdef89749
Vedanshi Shah <shah.ve@northeastern.edu>
2025-05-08 23:20:19 -0400

    start

---
+++ azza-jewels/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ azza-jewels/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">Crafting Legacy, Delivering Excellence</h1>
+        <p className="hero-subtitle">
+          Explore the finest 18K, 21K, and 22K Gold Jewelry from Oman.
+        </p>
+        <button className="hero-button">Shop Now</button>
+      </div>
+    </div>
+  );
+}
